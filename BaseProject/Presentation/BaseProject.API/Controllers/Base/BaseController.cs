de45c0dbca13f01c4e12983864acd48f9b0d20bc
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-07-25 22:09:37 +0300

    BaseController added

--- BaseProject/Presentation/BaseProject.API/Controllers/Base/BaseController.cs
+++ BaseProject/Presentation/BaseProject.API/Controllers/Base/BaseController.cs
@@ -1,12 +1,22 @@
 ﻿using BaseProject.Application.Interfaces.Mapper;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BaseProject.API.Controllers.Base
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BaseController<T> : ControllerBase where T : BaseController<T>
+    public abstract class BaseController<T> : ControllerBase where T : BaseController<T>
     {
-   
+        private IMediator mediator;
+        private ILogger<T> logger;
+        private IMapper mapper;
+
+        protected IMediator Mediator
+            => mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+        protected ILogger<T> Logger 
+            => logger ??= HttpContext.RequestServices.GetService<ILogger<T>>();
+        protected IMapper Mapper
+            => mapper ??= HttpContext.RequestServices.GetService<IMapper>();
     }
 }
--- BaseProject/Presentation/BaseProject.API/Controllers/TestController.cs
+++ BaseProject/Presentation/BaseProject.API/Controllers/TestController.cs
@@ -1,61 +1,40 @@
-﻿using BaseProject.Application.Features.Queries.Products.GetAllProducts;
-using BaseProject.Application.Interfaces.Repositories.Common;
+﻿using BaseProject.API.Controllers.Base;
 using BaseProject.Application.Interfaces.Services.Tokens;
 using BaseProject.Domain.Filtering;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using SendGrid.Helpers.Errors.Model;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Json;
-using System.Web;
 
 namespace BaseProject.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TestController : ControllerBase
-    {
-        private readonly IMediator mediator;
-        private readonly ITokenService tokenService;
 
-        public TestController(IMediator mediator, ITokenService tokenService)
-        {
-            this.mediator = mediator;
-            this.tokenService = tokenService;
-        }
+    public class TestController : BaseController<TestController>
+    {
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            string token = await tokenService.GenerateTokenAsync(Guid.NewGuid().ToString());
-            return Ok(token);
+             return Ok();
         }
 
         [HttpPost("[action]")]
         public async Task<IActionResult> GetWithFilterAsync(QueryParameters queryParameters)
         {
-            var products = await mediator.Send(new GetAllProductsRequest { QueryParameters = queryParameters });
-            return Ok(products);
+            return Ok();
         }
 
         [HttpPost("/a")]
         public IActionResult Post([FromForm] int a)
         {
-            throw new UnauthorizedException("Test exception");
-
             return Ok();
         }
 
         [HttpPut]
         public IActionResult Put()
         {
-            throw new ForbiddenException("Test exception");
             return Ok();
         }
 
         [HttpDelete]
         public IActionResult Delete()
         {
-            throw new ValidationException();
             return Ok();
         }
 
