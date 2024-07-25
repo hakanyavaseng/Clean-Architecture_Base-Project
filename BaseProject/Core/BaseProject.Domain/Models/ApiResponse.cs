7a18997884b41af12118efa90b29bb28c37cc4d7
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-07-25 22:11:54 +0300

    ApiResponse added.

---
+++ BaseProject/Core/BaseProject.Domain/Models/ApiResponse.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BaseProject.Domain.Models
+{
+    public record ApiResponse<T> 
+    {
+        public bool Success { get; init; }
+        public string? Message { get; init; }
+        public int StatusCode { get; init; } = StatusCodes.Status200OK;
+        public T? Data { get; init; }
+    }
+}
